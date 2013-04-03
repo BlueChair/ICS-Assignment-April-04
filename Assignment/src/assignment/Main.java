89db7997fb915d3c701ed8d80dcff44da3be0090
Kadin <KadinChoi@gmail.com>
2013-04-03 14:16:02 -0400

    Tests Run ... works!
    You will see ... :D

---
+++ Assignment/src/assignment/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package assignment;
+
+/**
+ *
+ * @author choik8962
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello Aron!");
+    }
+
+}
