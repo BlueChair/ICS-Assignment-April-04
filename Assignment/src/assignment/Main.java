9e85f714d72eb17c3034015f27154fe06ec693fa
K <Kadin_95@hotmail.com>
2013-04-04 12:49:00 -0400

    Started Player Class

--- Assignment/src/assignment/Main.java
+++ Assignment/src/assignment/Main.java
@@ -4,7 +4,7 @@
  */
 
 package assignment;
-
+import java.util.*;
 /**
  *
  * @author choik8962
@@ -15,7 +15,16 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello Aron!");
+        Scanner in = new Scanner(System.in);
+        String name;
+        int cash;
+
+        System.out.println("What is your name?");
+        name = in.next();
+        System.out.println("How much money do you have?");
+        cash = in.nextInt();
+        Player player1 = new Player(name, cash);
+
     }
 
 }
---
+++ Assignment/src/assignment/Player.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package assignment;
+
+/**
+ *
+ * @author choik8962
+ */
+public class Player {
+
+}
