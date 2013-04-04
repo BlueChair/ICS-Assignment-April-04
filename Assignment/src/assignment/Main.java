b3a542e282052b2f843cb56ad3ab0ea533563e71
K <Kadin_95@hotmail.com>
2013-04-04 12:56:57 -0400

    PLayer class change

--- Assignment/src/assignment/Main.java
+++ Assignment/src/assignment/Main.java
@@ -19,11 +19,14 @@ public class Main {
         String name;
         int cash;
 
+        System.out.println("Welcome to the Carnival!! We have 3 games that you can play!");
+        System.out.println("The games are: ");
         System.out.println("What is your name?");
         name = in.next();
         System.out.println("How much money do you have?");
         cash = in.nextInt();
         Player player1 = new Player(name, cash);
+        System.out.println("What game do you want to play?");
 
     }
 
